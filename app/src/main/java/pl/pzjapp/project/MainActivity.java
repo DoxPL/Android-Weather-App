a687a8f31f3b8d1b7b96aa2d9ebc49dea67d6598
Dominik Galoch <dox100@gmail.com>
2019-02-24 21:58:36 +0100

    Removed floating btn

--- app/src/main/java/pl/pzjapp/project/MainActivity.java
+++ app/src/main/java/pl/pzjapp/project/MainActivity.java
@@ -25,12 +25,12 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             }
-        });
+        }); */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
